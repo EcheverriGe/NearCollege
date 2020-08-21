d0da5a8ff75595d100ded09a25cc881fdf350651
StefaniaMartinezT <niaamt1880@gmail.com>
2020-08-21 08:20:21 -0500

    Diseño
    Iconos

--- NC/NC/Controllers/AccesoController.cs
+++ NC/NC/Controllers/AccesoController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using NC.Models;
+
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography;
 using System.Text;
